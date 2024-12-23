c46995027d4f1bfb019a9023ba0c7bd2a3690871
ari <kjk9858@gmail.com>
2024-12-24 01:01:44 +0900

    푸터수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,22 @@ import React from 'react';
 import '../style/Footer.scss'
 
 const Fotter = () => {
+
+  const frontUrl = process.env.REACT_APP_FRONT_URL;
+  const backUrl = process.env.REACT_APP_BACK_URL;
+  const blogUrl = process.env.REACT_APP_BLOG_URL;
     return (
-<footer class="footer">
-  <div class="footer-container">
-    <div class="footer-info">
-      <p class="company-name">© 2024 TaskHub-Project.</p>
-      <p class="company-phone">Tel: +1 (123) 456-7890</p>
-      <p class="company-email">Email: dkfl093@naver.com</p>
+<footer className="footer">
+  <div className="footer-container">
+    <div claclassNamess="footer-info">
+      <p className="company-name">© 2024 TaskHub-Project.</p>
+      <p className="company-phone">Tel: +1 (123) 456-7890</p>
+      <p className="company-email">Email: dkfl093@naver.com</p>
     </div>
     <div class="footer-social">
-      <a href="#" class="social-link">GitHub-Front</a>
-      <a href="#" class="social-link">GitHub-Back</a>
-      <a href="#" class="social-link">Blog</a>
+      <a href={`${frontUrl}`} className="social-link" target="_blank" rel="noopener noreferrer">GitHub-Front</a>
+      <a href={`${backUrl}`} className="social-link" target="_blank" rel="noopener noreferrer">GitHub-Back</a>
+      <a href={`${blogUrl}`} className="social-link" target="_blank" rel="noopener noreferrer">Blog</a>
     </div>
   </div>
 </footer>
